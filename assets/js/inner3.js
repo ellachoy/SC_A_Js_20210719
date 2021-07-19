10535a4a43c97f519d97b0b09a9a16716c69792e
Ella Choy <egal21de@gmail.com>
2021-07-19 20:59:38 +0200

    innerhtml

--- assets/js/inner3.js
+++ assets/js/inner3.js
@@ -1,1 +1,20 @@
-document.getElementById("gallery").innerHTML="<img src="https://unsplash.it/200/">
+document.getElementById("gallery").innerHTML=  '<figure style="display:inline-block"><img src="https://unsplash.it/200/"><figcaption>Fig. 1</figcaption></figure>';
+document.getElementById("gallery").innerHTML += '<figure style="display:inline-block"><img src="https://unsplash.it/200/"><figcaption>Fig. 2</figcaption></figure>';
+document.getElementById("gallery").innerHTML += '<figure style="display:inline-block"><img src="https://unsplash.it/200/"><figcaption>Fig. 3</figcaption></figure>';
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById("gallery").innerHTML=" <figure><img src="https://unplash.it/300" alt="#"><figcaption>Fig.1</figcaption></figure>"
+
+// document.getElementById("gallery").innerHTML=" <figure><img src="https://unplash.it/300" alt="#"><figcaption>Fig.2</figcaption></figure>"
+
